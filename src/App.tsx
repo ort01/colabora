f1177480130032ebc604080e67d9f0f332112e7e
ort01 <ortutova.kristina@gmail.com>
2023-11-22 17:28:14 +0100

    authReducer with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import './App.scss'
+//redux
+import { useDispatch, useSelector } from 'react-redux'
+import { authState, selectAuthReady } from './redux/authReducer/auth'
+
+//firebase
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase/config'
+
 
 function App() {
+  const dispatch = useDispatch()
+  const authReady = useSelector(selectAuthReady)
+
+  console.log(authReady);
+
+  const unsub = onAuthStateChanged(auth, (user) => {
+    dispatch(authState(user))
+    unsub()
+  })
+
 
   return (
-    <div className='App'>
-      Hello
-    </div>
+    <>
+      {authReady &&
+        <div className='App'>
+          Hello
+        </div>
+      }
+    </>
   )
 }
 
