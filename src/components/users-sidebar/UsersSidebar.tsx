46ef63f3c391d7c5d5b00eff562f9f80e887358b
ort01 <ortutova.kristina@gmail.com>
2024-01-25 10:03:12 +0100

    fixed type error in users sidebar

--- src/components/users-sidebar/UsersSidebar.tsx
+++ src/components/users-sidebar/UsersSidebar.tsx
@@ -1,17 +1,19 @@
 import "./UsersSidebar.scss"
 //hooks
 import { useCollection } from "../../hooks/useCollection"
+import { UserDocument } from "../../interfaces/Collections"
 import Avatar from "../avatar/Avatar"
+
 
 export default function UsersSidebar() {
 
-    const { documents, error } = useCollection("users")
+    const { documents, error } = useCollection<UserDocument>("users")
 
     return (
         <div className="users-list">
             <h2>All users</h2>
             {error && <div className="error">{error}</div>}
-            {documents && documents.map((user) => (
+            {documents && documents.map((user: UserDocument) => (
                 <div key={user.id} className="users-list__user">
                     {user.online && <span className="online" />}
                     <p>{user.displayName}</p>
